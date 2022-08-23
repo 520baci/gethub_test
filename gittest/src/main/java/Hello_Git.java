c601a19899f10b9b470eed5ac0a73bdbfae09789
查博 <2995771387@qq.com>
2022-08-23 18:57:54 +0800

    create hello

--- gittest/src/main/java/Hello_Git.java
+++ gittest/src/main/java/Hello_Git.java
@@ -5,5 +5,7 @@
 public class Hello_Git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }

7070cd70f4172cc8d252f9536fb42b9c0465ecbe
kukou <1025568745@qq.com>
2022-08-23 20:22:04 +0800

    来自刘东运的好评

---
+++ gittest/src/main/java/ZhaZhaHui.java
@@ -0,0 +1,9 @@
+public class ZhaZhaHui {
+    public static void main(String[] args) {
+        System.out.println("查博喜欢熬夜学习");
+        System.out.println("查博喜欢熬夜学习");
+        System.out.println("查博喜欢熬夜学习");
+        System.out.println("查博喜欢熬夜学习");
+        System.out.println("查博喜欢熬夜学习");
+    }
+}
